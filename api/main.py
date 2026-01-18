b8784babdfb0957a91713b5b6d545e1888cfde35
Syura_Krm <nextstageof@gmail.com>
2026-01-18 21:44:38 +0900

    add sample ui

--- api/main.py
+++ api/main.py
@@ -1,9 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
+
 from core.build_data import load_abilities
+
+class CalcInput(BaseModel):
+    message: str
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request}
+    )
+
 @app.post("/calculate")
-def calculate_api(payload: dict):
-    result = load_abilities(payload)
-    return result
+def calculate(data: CalcInput):
+    return {
+        "received": data.message,
+        "status": "ok"
+    }
+    #result = load_abilities(payload)
+    #return result
