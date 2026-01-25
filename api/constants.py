38559847a5e1c60fdffdf540d56e39bce1e61510
Syura_Krm <nextstageof@gmail.com>
2026-01-25 17:34:39 +0900

    add consts file for python

---
+++ api/constants.py
@@ -0,0 +1,14 @@
+SOURCE_RULES = {
+    "battle": {
+        "stack_group": "battle"
+    },
+    "ultimate": {
+        "stack_group": "ultimate"
+    },
+    "ex": {
+        "stack_group": "battle"
+    },
+    "support": {
+        "stack_group": "support"
+    },
+}
